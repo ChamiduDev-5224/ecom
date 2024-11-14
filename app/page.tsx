9e0c9b3535283f2b24f1ca279f02ac1cbdd7e1eb
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-11-14 21:39:29 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,31 @@
 import Image from "next/image";
+import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+   <>
+      <div className="bg-black">
+        <p className="text-center text-sm md:text-base text-brand-font-white p-2">Sign up and get 20% off to your first order <span className="font-semibold underline cursor-pointer">Sign Up Now</span></p>
+      </div>
+      
+      <div className="flx p-6 justify-between">
+          <div className="flx gap-3 place-items-center">
+            <Image src="/icons/menu.svg" className="pointer md:hidden" width={20} height={20} alt="mobile-menuIcon"/>
+            <p className="text-black font-extrabold uppercase text-2xl ml-4">shop.co</p>
+            <span className="pointer hover:text-violet-300 ml-10">Shop</span>
+            <Image src="/icons/arrowDown.svg" className="pointer" width={16} height={16} alt="mobile-menuIcon"/>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <span className="pointer hover:text-violet-300 ml-2">On Sale</span>
+            <span className="pointer hover:text-violet-300 ml-2">New Arrivals</span>
+            <span className="pointer hover:text-violet-300 ml-2">Brands</span>
+          </div>
+          <div className="flx gap-2">
+            <input className="bg-brand-bg-serbg border-0 rounded-lg pl-4 text-sm" placeholder="Search For Products"/>
+            <Image src="/icons/search.svg" className="pointer md:hidden" width={20} height={20} alt="mobile-menuIcon"/>
+            <Image src="/icons/cart.svg" className="pointer" width={20} height={20} alt="mobile-menuIcon"/>
+            <Image src="/icons/user.svg" className="pointer" width={20} height={20} alt="mobile-menuIcon"/>
+          </div>
+      </div>
+   </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        brand:{
+          bg:{
+            main:"#9A9A9A",
+            sub:"#F2F0F1",
+            card:"#F0EEED",
+            serbg:"#F0F0F0",
+
+          },
+          font:{
+            black:"#000000",
+            white:"#FFFFFF",
+
+          }
+        }
       },
     },
   },
